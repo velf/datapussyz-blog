c6753001a07f7a645292c5e432e201608dc92e51
welf <orsolya.vasarhelyi@gmail.com>
2016-07-14 16:23:51 +0200

    add forms, edit post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +10,18 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return post.redirect('post_detail', pk=post.pk)
+            #return HttpResponseRedirect(reverse('post_detail', args=[pk]))    
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
